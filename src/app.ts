6a6a97ba9b1a4650ca6f6ffd2f97fce96c3166d1
CarlosRBF <carlosrblanco00@gmail.com>
2023-02-05 13:05:28 -0300

    feat: swagger docs

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
-import express from "express"
 import "express-async-errors"
+import express from "express"
+import swaggerUi from "swagger-ui-express"
+import swaggerDocs from "./swagger.json"
 import { appRoutes } from "./routes"
 import { errorMiddleware } from "./middlewares/errors.middleware"
 
 const app = express()
 app.use(express.json())
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 appRoutes(app)
 app.use(errorMiddleware)
-
 
 export default app
